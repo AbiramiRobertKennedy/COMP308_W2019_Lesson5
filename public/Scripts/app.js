690756b61ce87a0de1ce3057759a24686d62c1f7
Abirami <abiramirobertkennedy@gmail.com>
2019-02-04 18:49:55 -0800

    Added bootstrap and font awesome

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,12 @@
-/* Javascript goes here */
+//IIFE
+
+(function(){
+
+    function Start() {
+      console.log(`%c App Started...`,"font-size: 20px; color: blue; font-weight: bold");
+      
+    }
+
+    window.addEventListener("load", Start);
+
+})();
