21fda48d774d4fe62c968798275813170b216ba2
Abirami <abiramirobertkennedy@gmail.com>
2019-02-23 09:05:31 -0800

    added create functionality to db

--- routes/contact.js
+++ routes/contact.js
@@ -4,11 +4,11 @@ let mongoose = require('mongoose');
 
 //create a reference to the db schema
 
-let contact = require('../models/contact');
+let contactModel = require('../models/contact');
 
 /*Get Contact List page*/
 router.get('/',(req,res,next)=>{
-    contact.find((err, contactList) => {
+    contactModel.find((err, contactList) => {
          if(err){
              return console.error(err);
          } 
@@ -24,6 +24,36 @@ router.get('/',(req,res,next)=>{
              
          }   
     });
+});
+
+/* Get router for the add page*/
+router.get('/add',(req,res,next) => {
+     res.render('contacts/add',{
+        title: 'Add new contact'
+});
+
+});
+
+/* Post route for the add page */
+router.post('/add',(req,res,next) => {
+ 
+    let newContact = contactModel({
+        "name":req.body.name,
+        "desc":req.body.desc
+  
+ });
+
+ contactModel.create(newContact, (err, contactModel)=>{
+     if(err){
+         console.log(err);
+         res.end(err);
+     }
+     else {
+         //refresh the contact list
+         res.redirect('/contact-list');
+     }
+
+ });
 });
 
 module.exports = router;
